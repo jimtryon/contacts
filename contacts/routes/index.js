cdaf11ac10d86b39c8e29b90413579c1dcd97832
Jim Tryon <jtryon@mail.greenriver.edu>
2015-03-05 09:13:03 -0800

    Added route for contact list, view to show list of contacts

--- contacts/routes/index.js
+++ contacts/routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET hello world page. */
+router.get('/hello', function(req, res, next) {
+  res.render('helloworld', { title: 'Hello World' });
+});
+
+/* GET hello world page. */
+router.get('/aboutme', function(req, res, next) {
+  res.render('aboutme', { title: 'About Me' });
+});
+
+/* GET contact list page. */
+router.get('/contactlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('contacts');
+    collection.find({}, {}, function(e, docs) {
+        res.render('contactlist', {
+            "contactlist":docs
+        });
+    });
+});
+
 module.exports = router;
--- contacts/routes/users.js
+++ contacts/routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', { title: 'Express' });
+});
+
+/* GET contacts page */
+router.get('/contactlist', function(req, res) {
+	var db = req.db;
+	var collection = db.get('contacts');
+	collection.find({}, {}, function(e, docs) {
+		res.render('contactlist', {
+			"contactlist" : docs
+		});
+	});
+
 });
 
 module.exports = router;
